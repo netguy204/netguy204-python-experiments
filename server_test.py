8eeee946547b2bb9ccfef96b3d3abdcf22a6c5c5
Brian Taylor <btaylor@buster.wubo.org>
2009-10-08 01:32:39 -0400

    a lot more stuff

--- server_test.py
+++ server_test.py
@@ -1,14 +1,33 @@
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+import tornado.escape
+import os
 
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
-    self.write("Hello world")
+    results = []
+    with os.popen('git log --pretty=oneline') as f:
+      for line in f:
+        words = line.split(' ')
+        results.append({ 'sha1' : words[0], 'log' : ' '.join(words[1:]).rstrip() } )
+        self.set_header("Content-Type", "text/javascript")
+
+    json = tornado.escape.json_encode(results)
+
+    callback = self.get_argument("callback", default=0)
+    if callback:
+      json = "%s(%s);" % (callback, json)
+
+    self.write(json)
+
+settings = {
+    "static_path": os.path.join(os.path.dirname(__file__), "static")
+}
 
 application = tornado.web.Application([
     (r"/", MainHandler),
-])
+], **settings)
 
 if __name__ == "__main__":
   http_server = tornado.httpserver.HTTPServer(application)
